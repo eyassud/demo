71ea2534c66bd8e6bed41fdd2e39f56a78814813
daniel eyassu <danieleyassu@hotmail.com>
2026-06-12 14:56:56 -0400

    Section 2.7 completed.

--- src/Funny.svelte
+++ src/Funny.svelte
@@ -1,1 +1,11 @@
-<h2>No soap radio</h2>
+<script lang="ts">
+    export let fontSize = 100;
+    setInterval(() => {
+    if (fontSize === 150) {
+      fontSize = 5;
+    } else {
+      fontSize += 1;
+    }
+  }, 10);
+</script>
+<h2 style="font-size: {fontSize}px;">No soap radio.</h2>
---
+++ src/MissionPossible.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let color = "purple";
+</script>
+
+<h2 style="color: {color};">greenish</h2>
